fd11270c821486647a452089dfbb2b47fd4e9613
Craig Risien <crisien@coas.oregonstate.edu>
2014-05-14 16:14:27 -0700

    start of the FDC module

---
+++ ion_functions/data/fdc_functions.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""
+@package ion_functions.data.fdc_functions
+@file ion_functions/data/fdc_functions.py
+@author Craig Risien and Russell Desiderio
+@brief Module containing FDC related data-calculations.
+"""
